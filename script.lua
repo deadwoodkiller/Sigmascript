dc1b6f4fc5cb354142b49a73b0fb3ada10ba80f4
BTY <54715754+deadwoodkiller@users.noreply.github.com>
2025-02-10 20:24:25 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,20 @@
-if game.Players.LocalPlayer.UserId ~= 7743219187 then
-    Main:Notify({
-        Title = "Warning",
-        Content = "You are not the owner of this script!",
-        SubContent = "Please be careful!",
-        Duration = 5
-    })
-    task.wait(5)
-    Main:Notify({
-        Title = "Sigma",
-        Content = "Sigma!",
-        SubContent = "Sigma!",
-        Duration = 5
-    })
-    task.wait(2)
-    game.Players.LocalPlayer:Kick("kick")
+function InitLib(Main)
+    if game.Players.LocalPlayer.UserId ~= 7743219187 then
+        Main:Notify({
+            Title = "Warning",
+            Content = "You are not the owner of this script!",
+            SubContent = "Please be careful!",
+            Duration = 5
+        })
+        task.wait(5)
+        Main:Notify({
+            Title = "Sigma",
+            Content = "Sigma!",
+            SubContent = "Sigma!",
+            Duration = 5
+        })
+        task.wait(2)
+        game.Players.LocalPlayer:Kick("kick")
+    end
 end
+return InitLib
