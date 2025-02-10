af5fe26e8fb94b1f4681c1b1593eb42d6ad67453
BTY <54715754+deadwoodkiller@users.noreply.github.com>
2025-02-10 19:58:36 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,17 @@
+if game.Players.LocalPlayer.UserId ~= 7743219187 then
+    Main:Notify({
+        Title = "Warning",
+        Content = "You are not the owner of this script!",
+        SubContent = "Please be careful!",
+        Duration = 5
+    })
+    task.wait(5)
+    Main:Notify({
+        Title = "Sigma",
+        Content = "Sigma!",
+        SubContent = "Sigma!",
+        Duration = 5
+    })
+    task.wait(2)
+    game.Players.LocalPlayer:Kick("kick")
+end
